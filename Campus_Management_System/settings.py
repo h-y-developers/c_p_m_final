d1270e1efb1e94d881a439c34b81d95703622a77
Harsh Chakalasiya <harsh.chakalasiya@vayana.com>
2024-08-04 16:03:08 +0530

    FIx

--- Campus_Management_System/settings.py
+++ Campus_Management_System/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'harshchakalasiya0376@gmail.com'
-EMAIL_HOST_PASSWORD = 'zavrfbxlyklxggtn'
+EMAIL_HOST_USER = '<Email>'
+EMAIL_HOST_PASSWORD = '<Password>'
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'text-secondary',
